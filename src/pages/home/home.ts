ff2405ae47fd23510067c5490647fcc2ca5bf8f0
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-07 16:42:14 +0100

    add calculation logic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { PersonProvider } from '../../providers/person/person';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   user: any = {};
-  constructor(public navCtrl: NavController) {
-    this.user = { distance: 1000, age: 20 };
+  constructor(public navCtrl: NavController, person: PersonProvider) {
+
+    this.user = { distance: 1000, age: 20, gender: 'female' };
   }
   calculate() {
-    console.log(this.user);
+    this.person.age = this.user.age;
+    this.person.gender = this.user.gender;
+
+    this.person.doAssessment(this.user.distance);
+    console.log(this.person.assessmentMessage);
   }
 }
